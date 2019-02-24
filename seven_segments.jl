6c1085279de48258d7526dace3f2c197b1444d3f
unknown <ds16797@bristol.ac.uk>
2019-02-24 15:43:15 +0000

    work on storing in weights

---
+++ seven_segments.jl
@@ -0,0 +1,115 @@
+#Create a weight matrix of w_ij
+#Fix the weight values to store the three patterns
+#Write a function to evolve the network according to MP formula
+#Done synchronously so every node is updated at each time step
+#For each of the two test patterns, evolve the patterns until they
+#stop changing, printing the pattern at each step
+
+#Masters step
+#Add a function to calculate the energy of a configuration
+# E = -1/2 SIGMA_ij x_i w_ij x_j
+#Print out the energy of teh three learned patterns
+#Of the the tests patterns
+#Any patterns formed as the patterns are updated
+
+
+#Takes an 11 component vector and prints the corresponding seven segment
+function seven_segment(pattern::Array{Int64})
+
+    function to_bool(a::Int64)
+        if a==1
+            return true
+        end
+        false
+    end
+
+    function hor(d)
+        if d
+            println(" _ ")
+        else
+            println("   ")
+        end
+    end
+
+
+    function vert(d1,d2,d3)
+        word=""::String
+        if d1
+            word="|"
+        else
+            word=" "
+        end
+        if d3
+            word=string(word,"_")
+        else
+            word=string(word," ")
+        end
+        if d2
+            word=string(word,"|")
+        else
+            word=string(word," ")
+        end
+        println(word)
+
+    end
+
+    pattern_b=map(to_bool,pattern)
+
+    hor(pattern_b[1])
+    vert(pattern_b[2],pattern_b[3],pattern_b[4])
+    vert(pattern_b[5],pattern_b[6],pattern_b[7])
+
+    number=0
+    for i in 1:4
+        if pattern_b[7+i]
+            number+=2^(i-1)
+        end
+    end
+
+    println(number)
+
+end
+
+function hopfield(patterns::Array{Int64})
+    w = zeros(Float64, (11,11));
+
+    for i in 1:11
+        for j in 1:11
+            if i != j
+                sum = 0;
+                for p in 1:3
+                    sum += patterns[p,i] * patterns[p,j];
+                end
+                w[i,j] = (1 ÷ 3) * sum;
+            end
+        end
+    end
+end
+
+six=Int64[1,1,-1,1,1,1,1,-1,1,1,-1]
+three=Int64[1,-1,1,1,-1,1,1,1,1,-1,-1]
+one=Int64[-1,-1,1,-1,-1,1,-1,1,-1,-1,-1]
+
+seven_segment(three)
+seven_segment(six)
+seven_segment(one)
+
+#------------------
+
+hopfield([six,three,one]);
+
+println("test1")
+
+test=Int64[1,-1,1,1,-1,1,1,-1,-1,-1,-1]
+
+seven_segment(test)
+
+#here the network should run printing at each step
+
+println("test2")
+
+test=Int64[1,1,1,1,1,1,1,-1,-1,-1,-1]
+
+seven_segment(test)
+
+#here the network should run printing at each step
