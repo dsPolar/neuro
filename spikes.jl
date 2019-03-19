5479a83e2d4157f140599df140de764300f41caa
unknown <ds16797@bristol.ac.uk>
2019-03-19 16:37:28 +0000

    added cw2 stuff

---
+++ spikes.jl
@@ -0,0 +1,15 @@
+#spikes.jl
+
+#Q1 Calculate the Fano Factor of the SPIKE COUNT and CV of the INTER-SPIKE INTERVAL
+#   for 1000 seconds of spike train with firing rate 35Hz
+#   with refractory period 0 and 5ms
+#   Fano Factor should use windows of width 10,50,100ms
+
+#Q2 Calculate the Fano Factor and CV for spike train as above for rho.dat
+
+#Q3 Calculate and plot the spike triggered average for stim.dat
+
+#M1 Calulcate the Stimulus triggered by pairs of spikes
+#   For intevals of 2,10,20,50ms calculate the average stimulus before
+#   a pair of spikes seperated by that interval
+#   For adjacency case and non adjacent
