23f1382b59f78f38a8bda8b0269bd18ef2e3345f
dsPolar <ds16797@bristol.ac.uk>
2019-03-28 20:06:38 +0000

    written out fano factor code

--- spikes.jl
+++ spikes.jl
@@ -17,13 +17,39 @@
 include("load.jl")
 include("poisson.jl")
 using Printf
+using Statistics
 
 const global rho = "rho.dat"
 const global stim = "stim.dat"
 
+const global ms = 0.001
+const global sec = 1.0
+const global hz = 1.0
 
 # F = Var(SC)/Mean(SC) per window
-function fano(Vector{Float64}::spikes, wid::Float64)
+# Spike Train, window size in ms, spike train generation duration in ms
+function fano(Vector{Float64}::spikes, wid::Float64, big_t::Float64)
+
+    # Variable Initilisation
+    size_v = length(spikes)
+    max_t = spikes[size_v]
+    num_w = big_t/wid
+    tempVector = Vector{Float64}(0,num_w)
+    index = 1::Int64
+
+    # Filter counts into tempVector
+    for i in 1:num_w
+        if spikes[index] < i*wid
+            index++
+            tempVector[i]++
+        end
+    end
+
+    s_mean = mean(tempVector)
+    s_var = varm(tempVector,s_mean, corrected=false)
+
+    fanoFactor = s_var/s_mean
+    return fanoFactor
 
 end
 
@@ -39,15 +65,17 @@ end
 
 function queueOne()
     spikeTrain1 = [0.0]::Vector{Float64}
-    spikeTrain1 = get_spike_train(35.0, 1000.0, 0.0)
+    spikeTrain1 = get_spike_train(35.0*hz, 1000.0*sec, 0.0*ms)
     deleteat!(spikeTrain1,1)
-    ff1 = fano(spikeTrain1)
+
+    #ff1 = fano(spikeTrain1)
     coeff1 = cv(spikeTrain1)
 
     spikeTrain2 = [0.0]::Vector{Float64}
-    spikeTrain2 = get_spike_train(35.0, 1000.0, 5.0)
+    spikeTrain2 = get_spike_train(35.0*hz, 1000.0*sec, 5.0*ms)
     deleteat!(spikeTrain2,1)
-    ff2 = fano(spikeTrain2)
+
+    #ff2 = fano(spikeTrain2)
     coeff2 = cv(spikeTrain2)
 
 end
