08a74de80a357b00340e4675b940e488240a6bc5
dsPolar <ds16797@bristol.ac.uk>
2019-04-25 14:13:09 +0100

    joined neuron not working yet

--- linf.jl
+++ linf.jl
@@ -16,8 +16,8 @@
 
 
 using Printf
-using Statistics
 using PyPlot
+using Distributions
 
 const global tau_m = 0.01
 const global E_L = -0.07
@@ -51,12 +51,6 @@ function checkReset(voltage::Float64)
     end
 end
 
-#Return reset voltage
-function resetVoltage()
-    return V_r
-end
-
-
 #Use pyplot to plot voltages vector and save to file
 function plotVoltage(voltages::Vector{Float64})
     plot(voltages)
@@ -75,10 +69,9 @@ function q1()
     voltage[1] = V_r
     # 2:1001 to account for t[0] stored at voltage[1] due to Julia
     for t in 2:1001
-        if checkReset(voltage[t-1])
-            voltage[t] = voltageUpdate(resetVoltage())
-        else
-            voltage[t] = voltageUpdate(voltage[t-1])
+        voltage[t] = voltageUpdate(voltage[t-1])
+        if checkReset(voltage[t])
+            voltage[t] = V_r
         end
 
     end
@@ -89,5 +82,113 @@ end
 
 
 
+
+
+
+
+#Question 2
+# Simulate two neurons with synaptic connection between each other
+# First projects to second
+# Second projects to first
+
+const global tau_m2 = 0.02
+const global V_r2 = -0.08
+const global V_t2 = -0.054
+const global R_mI_e = 0.018
+const global R_mg_s = 0.15
+const global P = 0.5
+const global tau_s = 0.010
+
+#Simulate two cases
+# a) assuming synapses are excitatory with E_s = 0
+# b) assuming synapses are inhibitory with E_s = -0.08
+
+#Set the inital membrane potentials of neurons to different random values V_r:V_t
+#Simulate 1s of activity
+#Plot each case plot neurons together differentiated by colour
+
+
+function checkReset2(voltage::Float64)
+    if voltage > V_t2
+        return true
+    else
+        return false
+    end
+end
+
+function updateVoltages2(v1::Float64, v2::Float64, sUp::Float64, sDown::Float64, E_s::Float64)
+    update = Vector{Float64}(undef,2)
+
+    # Input voltage is from neuron 2
+    update[1] = ((E_L - v2) + (R_mI_e))    +    (R_mg_s * sDown * (E_s - v2))
+    update[1] = update[1] * tau_m2
+
+    # Input voltage is from neuron 1
+    update[2] = ((E_L - v1) + (R_mI_e))    +    (R_mg_s * sUp *   (E_s - v1))
+    update[2] = update[2] * tau_m2
+
+    return update
+end
+
+function plotVoltages2(v1::Vector{Float64}, v2::Vector{Float64}, file::String)
+    plot(v1,v2)
+    plt.title("Modelled Pair of Linked Leaky Integrate and Fire Neurons\n")
+    plt.xlabel("time/ms")
+    plt.ylabel("Voltage/V")
+    savefig(file)
+    println("File saved to ", file, "\n")
+end
+
+function q2()
+    voltage1 = zeros(1001)
+    voltage1[1] = rand(Uniform(V_r2,V_t2))
+    voltage2 = zeros(1001)
+    voltage2[1] = rand(Uniform(V_r2,V_t2))
+
+    synapseUp = 0.0::Float64
+    synapseDown = 0.0::Float64
+
+    update = Vector{Float64}(undef,2)
+
+    for t in 2:1001
+        update = updateVoltages2(voltage1[t-1], voltage2[t-1], synapseUp, synapseDown, 0.0)
+        voltage1[t] = update[1]
+        voltage2[t] = update[2]
+
+        if checkReset2(voltage1[t])
+            voltage1[t] = V_r2
+            synapseUp += P
+        end
+        if checkReset2(voltage2[t])
+            voltage2[t] = V_r2
+            synapseDown += P
+        end
+    end
+    plotVoltages2(voltage1,voltage2, "pairEx.jpg")
+
+    synapseUp = 0.0
+    synapseDown = 0.0
+
+    for t in 2:1001
+        update = updateVoltages2(voltage1[t-1], voltage2[t-1], synapseUp, synapseDown, -0.08)
+        voltage1[t] = update[1]
+        voltage2[t] = update[2]
+
+        if checkReset2(voltage1[t])
+            voltage1[t] = V_r2
+            synapseUp += P
+        end
+        if checkReset2(voltage2[t])
+            voltage2[t] = V_r2
+            synapseDown += P
+        end
+    end
+
+    plotVoltages2(voltage1, voltage2, "pairIn.jpg")
+end
+
+
+
 # Call question functions
 q1()
+q2()
